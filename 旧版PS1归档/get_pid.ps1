33f211a3da33d9af9c35b2f746175d722245ae98
摸王摸大王 <1229037592@qq.com>
2026-07-30 18:14:34 +0800

    Delete 旧版PS1归档 directory

--- 旧版PS1归档/get_pid.ps1
+++
@@ -1,2 +0,0 @@
-$ppid = (Get-WmiObject Win32_Process -Filter "ProcessId=$pid").ParentProcessId
-[IO.File]::WriteAllText("$env:TEMP\dnf_bat.pid", "$ppid")
--- 旧版PS1归档/存档管理.bat
+++
@@ -1,5 +0,0 @@
-chcp 65001
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0save-switch.ps1"
-pause
